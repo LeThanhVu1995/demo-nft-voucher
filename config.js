59607eae860ae7664280a6b3ea47bcc7184fb7a0
LeThanhVu1995 <lethanhvu20015@gmail.com>
2022-06-29 15:14:56 +0700

    update

---
+++ config.js
@@ -0,0 +1,6 @@
+export const nftaddress = "0xe3E8B35C34Ddda267b54ab896dB5ee6A16674faD";
+export const nftmarketplaceaddress =
+  "0x7727233cf0ea4869680339059b33005e53B563B2";
+export const nftpool = "0x7bA69bb46Fff9C484668B5b9028d8f59568E464a";
+
+// tooth record old explain lend because daughter brain much wet viable chronic
